4fce6428ae9461a9ee07f7ec02c9c12b51de8db5
kyle <kyleghafari@gmail.com>
2020-01-28 20:46:24 -0500

    Added core functionality. Still need to attepmt trycatch for validation.

--- Lab_8-Get_To_Know_Your_Classmates_Arrays/Program.cs
+++ Lab_8-Get_To_Know_Your_Classmates_Arrays/Program.cs
@@ -6,42 +6,84 @@ namespace Lab_8_Get_To_Know_Your_Classmates_Arrays
     {
         static void Main(string[] args)
         {
-            //so... we need
-            // 1) An set of student info... it's made up of
-            int studentIndex = 0;
-            //string[] studentNames = { "name0", "name1", "name2" };
-            //string[] favoriteFood = { "food0", "food1", "food2" };
-            //string[] homeTown = { "town0", "town1", "town2" };
-            //string[] whatever = { "whatever0", "whatever1", "whatever2", "whatever" };
-            string[] studentNames = { "name0", "name1", "name2" };
-            //Console.WriteLine($"Name: {studentNames[0]}, Favorite Food: {favoriteFood[0]}, Hometown {homeTown[0]}");
-            Console.WriteLine("Please choose a student you would like to learn more about: ");
-            //foreach (string stuname in studentNames)
-            //{
-            //    Console.Write($"{studentIndex+1}. ");
-            //    Console.WriteLine(stuname);
-            //}
-           
-            for (int i = 0; i < studentNames.Length; i++)
+            //arrays of student names/info
+            string[] studentNames = { "Kyle", "Erik", "Jenna", "Sam" };
+            string[] favoriteFood = { "Pizza", "Orange Chicken", "PEOPLE!", "Pasta" };
+            string[] homeTown = { "Lansing, MI", "Ann Arbor, MI", "Secrect Moon Base", "Toledo, OH" };
+            string[] hobbies = { "Playing drums", "Juggling", "REDACTED", "Disc Golf" };
+
+            string userContinue = "y";
+            while (userContinue == "y")
             {
-                Console.Write($"{i+1}. ");
-                Console.WriteLine(GetStudentInfo(studentIndex));
+                //print student names
+                Console.WriteLine("Please choose a student you would like to learn more about: ");
+                for (int i = 0; i < studentNames.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {studentNames[i]}");
+                }
+
+                //get user selection 1 (which student)
+                int userChoice;
+                string userChoiceAsString = Console.ReadLine();
+                bool userChoiceBool = int.TryParse(userChoiceAsString, out userChoice);
+                userChoice = userChoice - 1;
+
+                // selection 1 validation
+                while(!userChoiceBool || userChoice > studentNames.Length-1 || userChoice < 0)
+                {
+                    Console.WriteLine($"Please make a valid selection (1-{studentNames.Length})");
+                    userChoiceAsString = Console.ReadLine();
+                    userChoiceBool = int.TryParse(userChoiceAsString, out userChoice);
+                    userChoice = userChoice - 1;
+                }
+
+                // user has made selection 1. Give prompts for selection 2: favorite food, hometown or hobby
+                Console.WriteLine($"\nAlright, you have selected {studentNames[userChoice]}. What would you like to know about them?");
+                Console.WriteLine("You can ask about 1. Favorite food, 2. Hometown, or 3. Hobby.");
+
+                //print results based on selection 2. should only accept 1, 2, 3, ("favorite food", "hometown", or "hobby" as bonus. Add later)
+                int userChoice2;
+                string userChoiceAsString2 = Console.ReadLine();
+                bool userChoiceBool2 = int.TryParse(userChoiceAsString2, out userChoice2);
+                userChoice2 = userChoice2 - 1;
+
+                //selection 2 validation
+                while (!userChoiceBool2 || userChoice2 > 3 || userChoice2 < 0)
+                {
+                    Console.WriteLine("Please make a valid selection (1-3)");
+                    userChoiceAsString2 = Console.ReadLine();
+                    userChoiceBool2 = int.TryParse(userChoiceAsString2, out userChoice2);
+                    userChoice2 = userChoice2 - 1;
+                }
+
+                //print results based on selection 1(fav food), 2(hometown), 3(hobby)
+                if (userChoice2 == 0)
+                {
+                    Console.WriteLine($"{studentNames[userChoice]}'s favorite food is {favoriteFood[userChoice]}");
+                }
+                else if (userChoice2 == 1)
+                {
+                    Console.WriteLine($"{studentNames[userChoice]} is from {homeTown[userChoice]}");
+                }
+                else if (userChoice2 == 2)
+                {
+                    Console.WriteLine($"{studentNames[userChoice]}'s favorite hobby is {hobbies[userChoice]}");
+                }
+                else
+                {
+                    Console.WriteLine("Uh oh...");
+                }
+
+                //continue
+                Console.WriteLine("Would you like to continue? (y/n)");
+                userContinue = Console.ReadLine().ToLower();
+                while (userContinue != "y" && userContinue != "n")
+                {
+                    Console.WriteLine("Please enter 'y' or 'n'.");
+                    userContinue = Console.ReadLine();
+                }
             }
-                        
-                
-
-        }
-
-
-        public static string GetStudentInfo(int studentNumber)
-        {
-            string[] studentNames = { "Alex", "Kyle", "Frank", "Sarah" };
-            string[] favoriteFood = { "Pizza", "Meatballs", "Orange Chicken", "Apples" };
-            string[] homeTown = { "Lansing, MI", "Ann Arbor, MI", "New York, NY", "Boston, MA" };
-            string[] whatever = { "whatever0", "whatever1", "whatever2", "whatever" };
-
-            string stuOutput = ($"Name: {studentNames[studentNumber]}, Favorite Food: {favoriteFood[studentNumber]}, Hometown: {homeTown[studentNumber]}");
-            return stuOutput;
         }
     }
 }
+
